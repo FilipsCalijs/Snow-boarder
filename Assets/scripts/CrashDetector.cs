733b668c440de363ee898185525e90a18ddb0deb
FilipsCalijs <filips.calijs@gmail.com>
2024-09-08 22:57:02 +0300

    Using Invoke() For Delays

--- Assets/scripts/CrashDetector.cs
+++ Assets/scripts/CrashDetector.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class CrashDetector : MonoBehaviour
-{
+{ 
+    [SerializeField] float loadDelay = 0.5f;
+
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
-            Debug.Log("Ouch, hit my head");
+           
+            Invoke("ReloadScene", loadDelay);
         }    
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
