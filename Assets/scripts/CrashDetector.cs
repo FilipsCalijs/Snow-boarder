0add0b8d651046d1ea637e9f93bb8203031d7283
FilipsCalijs <filips.calijs@gmail.com>
2024-09-14 21:26:12 +0300

    How To Trigger Sound Effects

--- Assets/scripts/CrashDetector.cs
+++ Assets/scripts/CrashDetector.cs
@@ -7,12 +7,13 @@ public class CrashDetector : MonoBehaviour
 { 
     [SerializeField] float loadDelay = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
-
+    [SerializeField] AudioClip crashSFX;
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", loadDelay);
         }    
     }
@@ -22,3 +23,4 @@ public class CrashDetector : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 }
+
