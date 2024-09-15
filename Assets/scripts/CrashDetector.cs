b7292adc420ac7391daf4fec665f5ee208ba0433
FilipsCalijs <filips.calijs@gmail.com>
2024-09-15 15:58:24 +0300

    fix some bugs, game is ready

--- Assets/scripts/CrashDetector.cs
+++ Assets/scripts/CrashDetector.cs
@@ -5,13 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 { 
+    bool hasCrashed = false;
     [SerializeField] float loadDelay = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
     [SerializeField] AudioClip crashSFX;
     void OnTriggerEnter2D(Collider2D other) 
     {
-        if(other.tag == "Ground")
+      if(other.tag == "Ground" && !hasCrashed)
         {
+            hasCrashed = true;
+            FindObjectOfType<PlayerController>().DisableControls();
             crashEffect.Play();
             GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", loadDelay);
